6eb6d5e4ef59e6652f7f125bb317e063b1428d06
albericwalsh <albericwdsrt@gmail.com>
2024-09-10 16:34:23 +0200

    commit

--- index.php
+++ index.php
@@ -5,18 +5,74 @@
 <head>
     <meta charset="UTF-8">
     <title>Albéric Walsh</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;700&display=swap"
+          rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="Style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;700&display=swap" rel="stylesheet">
-
 </head>
 <body>
 <!-- background -->
-<div >
+<div>
     <img id="background" src="res/bg.jpg" alt="background">
 </div>
-<!-- end background -->
+<!-- menu -->
+<div class="menu">
+    <a href="#apropos" class="menu-btn">A PROPOS</a>
+    <a href="#contact" class="menu-btn">CONTACT</a>
+    <a href="#section3" class="menu-btn">ARTICLES</a>
+</div>
+<!-- content -->
+<div class="content">
+    <section id="apropos">
+        <div class="section-div">
+            <div class="profile-pic" style="position: absolute; top: 10vh;">
+                <img src="res/profile.jpg" alt="icon">
+            </div>
+            <h1 class="slide-enter" style="position: absolute; top: 10vh;">
+                Albéric
+            </h1>
+            <h2 class="slide-enter" style="position: absolute; top: 20vh;">
+                WALSH DE SERRANT
+            </h2>
+            <h3 class="slide-enter" style="position: absolute; top: 28vh;">
+                21 ans
+            </h3>
+            <h4 class="slide-enter" style="position: absolute; top: 35vh;">
+                06 17 84 51 96
+            </h4>
+<!--            attach to bottom of screen-->
+            <h3 class="slide-enter" style="position: absolute; bottom: 10vh; left: 5vw;">
+                Alternant Asset Manager à Arquus
+            </h3>
+            <h3 class="slide-enter" style="position: absolute; bottom: 4vh; left: 5vw;">
+                Etudiant à Ynov Paris
+            </h3>
+        </div>
+    </section>
+    <section id="contact">
+        <div style="display: flex">
+            <div style="width: 50%; padding: 5vh;">
+                <h1>
+                    Contact
+                </h1>
+                <p>
+                    <a href="mailto:albericwdsrt@gmail.com">mail</a>
+                </p>
+                <p>
+                    <a href="https://www.linkedin.com/in/alb%C3%A9ric-walsh-de-serrant-820a211ab/">linkedin</a>
+                </p>
+        </div>
+    </section>
+    <section id="section3">
+        <div>
+            <h1>
 
+            </h1>
+        </div>
+    </section>
+</div>
 
+<script src="js/active.js"></script>
 </body>
 
 </html>
---
+++ js/active.js
@@ -0,0 +1,46 @@
+const sections = document.querySelectorAll('section');
+const menuButtons = document.querySelectorAll('.menu-btn');
+
+function handleScroll() {
+    let index = 0;
+
+    sections.forEach((sections, i) => {
+        const sectionTop = sections.getBoundingClientRect().top;
+        const sectionHeight = sections.offsetHeight;
+
+        if (sectionTop < window.innerHeight && sectionTop + sectionHeight > 0) {
+            index = i;
+            sections.classList.add('visible');
+        }
+    });
+
+    menuButtons.forEach((btn, i) => {
+        if (i === index) {
+            btn.classList.add('active');
+        } else {
+            btn.classList.remove('active');
+        }
+    });
+}
+
+function Slide_Animate() {
+    const element = document.getElementById('.slide-enter');
+
+    element.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+        const elementHeight = element.offsetHeight;
+
+        if (elementTop < window.innerHeight && elementTop + elementHeight > 0) {
+            element.classList.add('active');
+        } else {
+            element.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('load', handleScroll); // Assure l'animation lors du chargement initial
+
+
+window.addEventListener('scroll', Slide_Animate);
+window.addEventListener('load', Slide_Animate);
