c3524f74405634d2450a71a1d5bed6745f281adb
albericwalsh <albericwdsrt@gmail.com>
2024-09-10 11:16:09 +0200

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Albéric Walsh</title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+<!-- background -->
+<div >
+    <img id="background" src="res/bg.jpg" alt="background">
+</div>
+<!-- end background -->
+
+
+</body>
+
+</html>
